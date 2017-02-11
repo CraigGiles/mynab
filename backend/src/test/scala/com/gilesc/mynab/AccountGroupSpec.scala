cf0ac96f6d14a660de09172ab9169749d877df87
Craig Giles <craig@gilesc.com>
2017-02-11 07:05:55 -0800

    AccountListSpec -> AccountGroupSpec

--- backend/src/test/scala/com/gilesc/mynab/AccountGroupSpec.scala
+++ backend/src/test/scala/com/gilesc/mynab/AccountGroupSpec.scala
@@ -12,11 +12,11 @@ trait MockAccountCreation {
     LoanAccount(AccountName(name), transactions)
 }
 
-class AccountListSpec extends TestCase with MockTransactionCreation with MockAccountCreation {
+class AccountGroupSpec extends TestCase with MockTransactionCreation with MockAccountCreation {
   import AccountGroup._
 
-  "Account lists" should {
-    "let me add new accounts" in {
+  "Account groups" should {
+    "allow the addition of new accounts" in {
       val al = List.empty[Account]
       val b = banking("chase", List(t(0.0, 1000.0)))
       val l = loan("chase", List(t(0.0, 1000.0)))
