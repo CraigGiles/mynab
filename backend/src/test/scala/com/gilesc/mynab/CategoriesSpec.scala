5490e9ef6719db5bcd90d2b8cc416dbeb3da7bdd
Craig Giles <craig@gilesc.com>
2017-02-12 07:32:33 -0800

    Added more comprehensive test cases

--- backend/src/test/scala/com/gilesc/mynab/CategoriesSpec.scala
+++ backend/src/test/scala/com/gilesc/mynab/CategoriesSpec.scala
@@ -2,7 +2,6 @@ package com.gilesc.mynab
 package category
 
 import com.gilesc.mynab.account._
-import com.gilesc.mynab.transaction.Transaction
 
 class CategorySpec extends TestCase
   with MockTransactionCreation
@@ -13,19 +12,30 @@ class CategorySpec extends TestCase
   "Renaming a category" should {
     "rename all categories within the transaction system" in {
       val changeme = "changeme"
+      val loans = "loans"
       val student = "student"
+      val mortgage = "mortgage"
 
       val transaction = trans(minorCategory = changeme)
-      val checkingAcc = banking("chase", List(transaction))
+      val stay = trans(minorCategory = mortgage)
+      val checkingAcc = banking("chase", List(transaction, stay))
 
-      val o = Category(MajorCategory("loans"), MinorCategory(changeme))
-      val n = Category(MajorCategory("loans"), MinorCategory(student))
-      val acc = AccountGroup(AccountName("budget accounts"), List(checkingAcc))
+      val before = Category(MajorCategory(loans), MinorCategory(changeme))
+      val after = Category(MajorCategory(loans), MinorCategory(student))
+      val group = AccountGroup(AccountName("budget accounts"), List(checkingAcc))
 
-      val newAccountGroup = renameCategory(o, n, acc)
+      val newAccountGroup = renameCategory(before, after, group)
 
       newAccountGroup.transactions.foreach { t =>
-        t.category.minor.value should be(student)
+        t.category.minor.value shouldNot be(changeme)
+
+        val one = t.category.minor.value == student
+        val two = t.category.minor.value == mortgage
+
+        // Assert that the minor category is either student or mortgage
+        // Since major category is the same, we want to make sure that
+        // only the proper minor categories get changed
+        assert(one || two)
       }
 
     }
