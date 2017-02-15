fb869d278ea445ad9a4fe63cc41a8a63b7ba2156
Craig Giles <craig@gilesc.com>
2017-02-15 05:56:49 -0800

    Commenting out the QfxSpec for now
    I need to refactor a little bit to ensure the client is easy to use by
    putting in an abstract layer in between the domain functions and the
    main method.

--- backend/src/test/scala/com/gilesc/mynab/parser/QfxSpec.scala
+++ backend/src/test/scala/com/gilesc/mynab/parser/QfxSpec.scala
@@ -5,15 +5,15 @@ import com.gilesc.mynab.transaction._
 
 import scala.io.Source
 
-class QfxSpec extends TestCase {
-  import Qfx._
+// class QfxSpec extends TestCase {
+//   import Qfx._
 
-  "QFX Parser" should {
-    "be able to take a file and return a list of transactions" in {
-      val readmeText = Source.fromResource("example.QFX").getLines()
-//      val file = new File("/example.qfx")
-//      val list = parse(file)
-      println(readmeText.toList)
-    }
-  }
-}
+//   "QFX Parser" should {
+//     "be able to take a file and return a list of transactions" in {
+//       val readmeText = Source.fromResource("example.QFX").getLines()
+// //      val file = new File("/example.qfx")
+// //      val list = parse(file)
+//       println(readmeText.toList)
+//     }
+//   }
+// }
