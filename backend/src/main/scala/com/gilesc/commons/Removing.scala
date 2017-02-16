25ef58a20561589edd809c7b0c1257b1418de05e
Craig Giles <craig@gilesc.com>
2017-02-16 04:43:05 -0800

    Adjusting removing

--- backend/src/main/scala/com/gilesc/commons/Removing.scala
+++ backend/src/main/scala/com/gilesc/commons/Removing.scala
@@ -1,9 +1,5 @@
 package com.gilesc.commons
 
-/**
-  * Created by craiggiles on 2/11/17.
-  */
 trait Removing {
-  def remove[T, F[_]]: (T, Vector[T]) => Vector[T] = (t, s) => s.filterNot(tr => tr == t)
-
+  def remove[T]: (T, Vector[T]) => Vector[T] = (t, s) => s.filterNot(tr => tr == t)
 }
