a534e12c301d647da9d34e9ae329ef61f0af3c13
Craig Giles <craig@gilesc.com>
2017-02-18 05:30:23 -0800

    First pass at a service aggregation layer
    Trying to experiment with partial application as a means of dependency
    injection. I don't want to go down the rabbit hole of reader monad quite
    yet, trying to see if this would be easier for engineers new to the
    language to grasp

--- backend/src/main/scala/com/gilesc/mynab/AccountService.scala
+++ backend/src/main/scala/com/gilesc/mynab/AccountService.scala
@@ -1,18 +1,51 @@
 package com.gilesc.mynab
 package account
 
+import cats.Monad
+import cats.data.{Kleisli, ReaderT}
+
+import scala.concurrent.Future
+
 case class AccountDetails(accountName: String, accountType: String, groupName: String)
 
 sealed trait AddAccountResult
 final case class Success(account: Account) extends AddAccountResult
 final case class Failure(message: String) extends AddAccountResult
 
-trait AccountService {
+sealed trait PersistenceResult
+case object PersistenceSuccessful extends PersistenceResult
+case class PersistenceFailure(message: String) extends PersistenceResult
+
+case class Config()
+
+trait AccountServiceModule {
+  def create: AccountDetails => AddAccountResult
 }
 
-object AccountService extends AccountService {
+trait AccountRepositoryModule {
+  def save: Account => PersistenceResult
+}
 
-  def addNewAccount(details: AccountDetails): AddAccountResult = {
+trait LoggingModule {
+  def log: String => Unit
+}
+
+object InMemoryAccountRepository extends AccountRepositoryModule {
+  def save: Account => PersistenceResult = { account =>
+    println(s"Persisting $account")
+    PersistenceSuccessful
+  }
+}
+
+object PrintlnLoggingService extends LoggingModule {
+  def log: String => Unit = println
+}
+
+object AccountService {
+
+  def create(persist: Account => PersistenceResult,
+             log: String => Unit)(details: AccountDetails): AddAccountResult = {
+
     val accountE = for {
       t <- AccountType(details.accountType)
       n <- AccountName(details.accountName)
@@ -20,15 +53,27 @@ object AccountService extends AccountService {
     } yield Account.create(t, n)
 
     accountE match {
-      case Left(s) => Failure(s.toString)
-      case Right(a) => Success(a)
+      case Left(s) =>
+        Failure(s.toString)
+
+      case Right(a) =>
+        persist(a) match {
+          case PersistenceSuccessful =>
+            Success(a)
+
+          case x =>
+            log(s"Persistence Failure: $x")
+            Failure(x.toString)
+        }
     }
   }
 }
 
-object Main extends App {
+object Main extends App with AccountServiceModule {
   val details = AccountDetails("My Account Name", "Banking", "No Group Name")
-  val account = AccountService.addNewAccount(details)
+
+  def create = AccountService.create(InMemoryAccountRepository.save, PrintlnLoggingService.log)
+  val account = create(details)
 
   println(account)
 }
