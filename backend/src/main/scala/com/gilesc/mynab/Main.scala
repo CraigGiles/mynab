2640a2d4364e18c100615cf26af98b4b9f382627
Craig Giles <craig@gilesc.com>
2017-03-10 04:38:28 -0800

    End of the refctor

--- backend/src/main/scala/com/gilesc/mynab/Main.scala
+++ backend/src/main/scala/com/gilesc/mynab/Main.scala
@@ -1,15 +1,4 @@
 package com.gilesc.mynab
 
-import com.gilesc.mynab.account._
-import com.gilesc.mynab.logging.PrintlnLoggingService
-import com.gilesc.mynab.repository.InMemoryAccountRepository
-
-object Main extends App with AccountServiceModule {
-  val details = AccountDetails(1L, "My Account Name", "Banking", "No Group Name")
-
-  def create = AccountService.create(InMemoryAccountRepository.save, PrintlnLoggingService)
-  def find = AccountService.find(InMemoryAccountRepository.find, PrintlnLoggingService)
-  val account = create(details)
-
-  println(account)
+object Main extends App {
 }
--- backend/src/main/scala/com/gilesc/mynab/account/AccountService.scala
+++ backend/src/main/scala/com/gilesc/mynab/account/AccountService.scala
@@ -9,62 +9,62 @@ import com.gilesc.mynab.transaction.Transaction
 import cats._
 import cats.implicits._
 
-sealed trait AccountActionResult
-final case class Success(account: Account) extends AccountActionResult
-final case class Failure(message: String) extends AccountActionResult
-
-case class AccountDetails(id: Long, accountName: String, accountType: String, groupName: String)
-case class FindDetails(name: String)
-
-trait AccountServiceModule {
-  def create: AccountDetails => AccountActionResult
-  def find: FindDetails => Option[Account]
-}
-
-object AccountService {
-
-  def create(save: Account => PersistenceResult,
-    log: LoggingModule)(details: AccountDetails): AccountActionResult = {
-
-    val results = convert(details) map { account =>
-      save(account) match {
-        case PersistenceSuccessful =>
-          Success(account)
-
-        case x =>
-          log.info(s"Persistence Failure: $x")
-          Failure(x.toString)
-      }
-    }
-
-    results match {
-      case Left(s) => Failure(s)
-      case Right(a) => a
-    }
-  }
-
-  def find(account: FindBy => Option[Account],
-    log: LoggingModule)(find: FindDetails): Option[Account] = {
-
-    log.info(s"Finding account by name: ${find.name}")
-    val either: Either[ValidationError, Option[Account]] = for {
-      n <- AccountName(find.name)
-    } yield account(FindByName(n))
-
-    either.toOption.flatten
-  }
-
-  val convert: AccountDetails => Either[String, Account] = { details =>
-    val i = AccountId(details.id)
-    val either = for {
-      t <- AccountType(details.accountType)
-      n <- AccountName(details.accountName)
-      g <- AccountName(details.groupName)
-    } yield Account.create(i, t, n)
-
-    either.left.map(_.toString)
-  }
-}
+//sealed trait AccountActionResult
+//final case class Success(account: Account) extends AccountActionResult
+//final case class Failure(message: String) extends AccountActionResult
+//
+//case class AccountDetails(id: Long, accountName: String, accountType: String, groupName: String)
+//case class FindDetails(name: String)
+//
+//trait AccountServiceModule {
+//  def create: AccountDetails => AccountActionResult
+//  def find: FindDetails => Option[Account]
+//}
+//
+//object AccountService {
+//
+//  def create(save: Account => PersistenceResult,
+//    log: LoggingModule)(details: AccountDetails): AccountActionResult = {
+//
+//    val results = convert(details) map { account =>
+//      save(account) match {
+//        case PersistenceSuccessful =>
+//          Success(account)
+//
+//        case x =>
+//          log.info(s"Persistence Failure: $x")
+//          Failure(x.toString)
+//      }
+//    }
+//
+//    results match {
+//      case Left(s) => Failure(s)
+//      case Right(a) => a
+//    }
+//  }
+//
+//  def find(account: FindBy => Option[Account],
+//    log: LoggingModule)(find: FindDetails): Option[Account] = {
+//
+//    log.info(s"Finding account by name: ${find.name}")
+//    val either: Either[ValidationError, Option[Account]] = for {
+//      n <- AccountName(find.name)
+//    } yield account(FindByName(n))
+//
+//    either.toOption.flatten
+//  }
+//
+//  val convert: AccountDetails => Either[String, Account] = { details =>
+//    val i = AccountId(details.id)
+//    val either = for {
+//      t <- AccountType(details.accountType)
+//      n <- AccountName(details.accountName)
+//      g <- AccountName(details.groupName)
+//    } yield Account.create(i, t, n)
+//
+//    either.left.map(_.toString)
+//  }
+//}
 
 // ---------------------------------------------------------------------------
 // NEW DOMAIN
