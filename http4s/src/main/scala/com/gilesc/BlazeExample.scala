2968748a3e419f9084df2f476ea04d6366b6aca2
Craig Giles <craig@gilesc.com>
2017-03-11 10:18:50 -0800

    An example of the http4s use

--- http4s/src/main/scala/com/gilesc/BlazeExample.scala
+++ http4s/src/main/scala/com/gilesc/BlazeExample.scala
@@ -1,18 +1,16 @@
 package com.gilesc
+package mynab
 
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.{Server, ServerApp}
 
 import scalaz.concurrent.Task
 
-/**
-  * Created by craiggiles on 2/17/17.
-  */
 object BlazeExample extends ServerApp {
   override def server(args: List[String]): Task[Server] = {
     BlazeBuilder
       .bindHttp(8080)
-      .mountService(HelloWorld.service, "/")
+      .mountService(HelloWorld.mynab, "/")
       .start
   }
 }
