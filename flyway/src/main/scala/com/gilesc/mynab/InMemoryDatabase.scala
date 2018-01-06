80daacd067203557f0167b496fc53c101e812652
Craig Giles <craig@gilesc.com>
2018-01-06 07:00:55 -0800

    Moved hard coded test db to config

--- flyway/src/main/scala/com/gilesc/mynab/InMemoryDatabase.scala
+++ flyway/src/main/scala/com/gilesc/mynab/InMemoryDatabase.scala
@@ -4,21 +4,25 @@ package mynab
 import com.gilesc.mynab.testkit.TestCase
 
 abstract class InMemoryDatabase extends TestCase {
-
   private[this] def migrateDatabase(): Unit = {
     import org.flywaydb.core.Flyway
+
+    case class DatabaseConfig(
+      driver: String,
+      url: String,
+      username: String,
+      password: String
+    )
+
+    val config = pureconfig.loadConfigOrThrow[DatabaseConfig]("mynab.database")
     val flyway = new Flyway()
-    lazy val databaseUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
-    lazy val databaseUser = "sa"
-    lazy val databasePassword = ""
 
     flyway.setLocations("classpath:db/migration")
-
-    flyway.setDataSource(databaseUrl, databaseUser, databasePassword)
-
+    flyway.setDataSource(config.url, config.username, config.password)
     flyway.migrate()
 
     ()
   }
+
   migrateDatabase()
 }
