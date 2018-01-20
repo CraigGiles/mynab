3a4d8d33cbcbc0478710d3e311aac1611b935c4f
Craig Giles <craig@gilesc.com>
2018-01-20 09:06:01 -0800

    Uncommenting unit test
    This test needs fixing.. spent more of my time on the actual integration
    with the integration tests

--- service/src/test/scala/com/gilesc/mynab/service/CategoryServiceSpec.scala
+++ service/src/test/scala/com/gilesc/mynab/service/CategoryServiceSpec.scala
@@ -22,34 +22,34 @@ object IOAssertion {
 // }
 
 class CategoryServiceSpec extends TestCase {
-  // def categories: CreateCategoryContext => IO[Either[RepositoryError, Category]] =
-  //   ctx => IO.pure(Left(RepositoryError.DuplicateKey))
+  val groups = new CategoryGroupRepository[IO] {
+    override def create(
+      ctx: CategoryGroupContext
+    ): IO[Either[RepositoryError, CategoryGroup]] = {
+      IO.pure(Left(RepositoryError.DuplicateKey))
+    }
+    override def find(name: CategoryName): IO[Option[CategoryGroup]] =
+      IO.pure(None)
+  }
 
-  // val groups = new CategoryGroupRepository[IO] {
-  //   override def create(
-  //     ctx: CategoryGroupContext
-  //   ): IO[Either[RepositoryError, CategoryGroup]] = {
-  //     IO.pure(Left(RepositoryError.DuplicateKey))
-  //   }
-  // }
-
-  // val categories = new CategoryRepository[IO] {
-  //   override def create(
-  //     ctx: CategoryContext
-  //   ): IO[Either[RepositoryError, Category]] = {
-  //     IO.pure(Left(RepositoryError.DuplicateKey))
-  //   }
-  // }
+  val categories = new CategoryRepository[IO] {
+    override def create(
+      ctx: CategoryContext
+    ): IO[Either[RepositoryError, Category]] = {
+      IO.pure(Left(RepositoryError.DuplicateKey))
+    }
+  }
 
 
-  // behavior of "Category Service"
-  // it should "allow me to create a new category" in {
-  //   val service = CreateCategoryService.apply[IO](groups, categories)
-  //   val food = CategoryName("Food")
-  //   val diningOut = CategoryName("Dining Out")
-  //   val ctx = CreateCategoryContext(food, diningOut)
-  //   val Left(result) = service(ctx).unsafeRunSync
-  //   result shouldBe a [RepositoryError.DuplicateKey.type]
-  // }
+  behavior of "Category Service"
+  it should "allow me to create a new category" in {
+    // TODO fix thix unit test
+    val service = CreateCategoryService.apply[IO](groups, categories)
+    val food = CategoryName("Food")
+    val diningOut = CategoryName("Dining Out")
+    val ctx = CreateCategoryContext(food, diningOut)
+    val Left(result) = service(ctx).unsafeRunSync
+    result shouldBe a [String]
+  }
 
 }
